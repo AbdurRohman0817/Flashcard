1c956159129994c037a7f2165c78c482c88fdc9a
jiannalee <152844111+jiannalee@users.noreply.github.com>
2024-01-15 15:53:45 +0900

    Create Flashcard.py

---
+++ Flashcard.py
@@ -0,0 +1,15 @@
+option = int(input("Choose 1,2,3 or 4 \n \n 1 = Go into folder \n 2 = Matching Game  \n 3 = Create Folder  \n 4 = Typing Game"))
+
+def option1():
+    pass
+
+def option2():
+    pass
+
+def option3():
+    pass
+
+def option4():
+    pass
+
+print = option
